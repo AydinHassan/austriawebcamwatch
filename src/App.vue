dfa8d9a71e8429a26a2f8bb03d8fdabe10bd92c8
Aydin Hassan <aydin@hotmail.co.uk>
2025-11-16 16:29:21 +0100

    Handle login properly

--- src/App.vue
+++ src/App.vue
@@ -41,7 +41,8 @@ import {
 } from '@/components/ui/dialog/index.js'
 import { Input } from '@/components/ui/input/index.js'
 import useSupabase from '@/composables/UseSupabase'
-
+import { Badge } from '@/components/ui/badge/index.js'
+const { supabase } = useSupabase();
 
 const mode = useColorMode()
 mode.value = 'dark'
@@ -170,7 +171,9 @@ provide('toggleWebcam', toggleWebcam);
 provide('switchPreset', switchPreset);
 provide('deletePreset', deletePreset);
 
-onMounted(() => {
+const user = ref(null);
+
+onMounted(async () => {
   const visited = repository.hasVisited();
 
   if (visited === false) {
@@ -192,6 +195,14 @@ onMounted(() => {
   }
 
   randomiseCams()
+
+  const { data } = await supabase.auth.getUser()
+  user.value = data.user ?? null;
+
+  supabase.auth.onAuthStateChange((_, session) => {
+    user.value = session?.user ?? null
+    console.log(user.value);
+  })
 })
 
 watch(selectedPresetName, (presetName) => {
@@ -215,20 +226,23 @@ const footerNavigation = [
 ]
 
 const loginOpen = ref(false);
-
 const handleLogin = async (provider) => {
   try {
-    const { supabase } = useSupabase();
-
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: provider,
     })
 
-    console.log('Logged in')
+    console.log(data)
   } catch (error) {
     alert(error.message);
   }
 };
+
+const handleLogout = async () => {
+  const { error } = await supabase.auth.signOut()
+
+  user.value = null;
+}
 </script>
 
 <template>
@@ -331,15 +345,20 @@ const handleLogin = async (provider) => {
           <Dialog v-model:open="loginOpen">
             <DialogTrigger as-child>
               <Button variant="outline" class="px-3 lg:px-4">
-                <PersonIcon></PersonIcon>
+                <PersonIcon :class="user ? 'text-green-500' : 'text-red-400'" />
               </Button>
             </DialogTrigger>
             <DialogScrollContent class="w-[90vw] sm:max-w-sm">
               <DialogHeader>
-                <DialogTitle>Login</DialogTitle>
+                <DialogTitle>Profile</DialogTitle>
               </DialogHeader>
               <div class="flex space-x-2 flex-col pt-6">
-                <Button variant="outline" type="button" @click="handleLogin('github')">
+                <div v-if="user" class="flex flex-col gap-4">
+                  <p v-if="user">Logged in as <Badge variant="secondary">{{user.email}}</Badge> via <Badge variant="outline">GitHub</Badge></p>
+
+                  <Button variant="destructive" type="button" @click="handleLogout">Logout</Button>
+                </div>
+                <Button v-else variant="outline" type="button" @click="handleLogin('github')">
                   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                     <path
                       d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
